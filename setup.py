9197aede07d728160a983a14cedd87ee2e429605
Дмитрий Головнёв <dmitry.golovnev@initflow.com>
2025-04-08 17:51:25 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-proxy-monitoring",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
 
     # author="",
